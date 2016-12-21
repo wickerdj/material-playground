7643ed19ffa3331999f3189b70d5c5951a0d3dde
wickerdj <wickerdj@gmail.com>
2016-12-20 18:49:51 -0600

    Completed Master/Detail Step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,31 @@ export class Hero {
   name: string;
 }
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Tour of Heros';
-  hero: Hero = { id: 1, name: 'Windstrom' };
+  heroes = HEROS;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
+const HEROS: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynamo' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+]
